3eb5741bf73642f6e28efb9eb06030e1e16db95a
aarrun <arun.sp212@gmail.com>
2022-11-14 04:30:25 +0530

    file rename

--- Run.bat
+++ Run.bat
@@ -1,4 +1,4 @@
-C:\Users\aruns\AppData\Local\Programs\Python\Python39\Scripts\pytest.exe -s -v -m "sanity" --html=./Reports/report_chrome.html
+C:\Users\aruns\AppData\Local\Programs\Python\Python39\Scripts\pytest.exe -s -v -m "sanity" --html=./Reports/report_chrome.html testCases/
 :: pytest -s -v -m "sanity or regression" --html=./Reports/report_chrome.html testCases/
 :: pytest -s -v -m "sanity and regression" --html=./Reports/report_chrome.html testCases/
 :: pytest -s -v -m "regression" --html=./Reports/report_chrome.html testCases/
